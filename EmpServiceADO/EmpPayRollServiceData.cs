d8c81d41f62edc9f88d8bb8c35d2c4d03e5be464
Imransk7768 <imransk7768@gmail.com>
2022-06-08 23:29:23 +0530

    [Imran] Added: Adding Employee Details

---
+++ EmpServiceADO/EmpPayRollServiceData.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpServiceADO
+{
+    public class EmpPayRollServiceData
+    {
+        private SqlConnection con;
+        public void Connection()
+        {
+            string connectingString = "Data Source=(localdb)\\MSSQLLocaldb;Initial Catalog=Employee_Payroll_DB";
+        }
+        public void AddEmployee(EmpModel emp) 
+        {
+            try
+            {
+                Connection();
+                SqlCommand command = new SqlCommand("spAddEmployee", con);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@Name", emp.Name);
+                command.Parameters.AddWithValue("@Age", emp.Age);
+                command.Parameters.AddWithValue("@Salary", emp.Salary);
+                command.Parameters.AddWithValue("@StartDate", emp.StartDate);
+                command.Parameters.AddWithValue("@Gender", emp.Gender);
+                command.Parameters.AddWithValue("@PhoneNumber", emp.PhoneNumber);
+                command.Parameters.AddWithValue("@Department", emp.Department);
+                command.Parameters.AddWithValue("@Deductions", emp.Deductions);
+                command.Parameters.AddWithValue("@Taxable_Pay", emp.Taxable_Pay);
+                command.Parameters.AddWithValue("@Income_Tax", emp.Income_Tax);
+                command.Parameters.AddWithValue("@Net_Pay", emp.Net_Pay);
+                con.Open();
+                int result = command.ExecuteNonQuery();
+                con.Close();
+                if (result != 0)
+                {
+                    Console.WriteLine("Data Added");
+                }
+                else
+                {
+                    Console.WriteLine("Data Not Added.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
--- EmpServiceADO/Program.cs
+++ EmpServiceADO/Program.cs
@@ -6,7 +6,32 @@ namespace EmpServiceADO
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to EmpPayRoll Service");
+            Console.WriteLine(" Welcome to Payroll Service");
+            EmpPayRollServiceData empData = new EmpPayRollServiceData();
+            Console.WriteLine("1. Insert Emp Details\n");
+            Console.Write("Enter option to Execute : ");
+            int choose = Convert.ToInt32(Console.ReadLine());
+            switch (choose)
+            {
+                case 1:
+                    EmpModel addMdl = new EmpModel();
+                    addMdl.Name = "Shashank";
+                    addMdl.Age = 33;
+                    addMdl.Salary = 6000;
+                    addMdl.StartDate = Convert.ToDateTime("07-02-2017");
+                    addMdl.Gender = "M";
+                    addMdl.PhoneNumber = "9123456777";
+                    addMdl.Department = "Sales";
+                    addMdl.Deductions = 500;
+                    addMdl.Taxable_Pay = 500;
+                    addMdl.Income_Tax = 500;
+                    addMdl.Net_Pay = 2000;
+                    empData.AddEmployee(addMdl);
+                    break;
+                default:
+                    Console.WriteLine("Execution Ends.");
+                    break;
+            }
         }
     }
 }
